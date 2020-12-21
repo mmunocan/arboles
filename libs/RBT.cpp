0d778183fe54db9fd8053237e3407ac4c0baf3d2
Carlos <carlos@localhost.localdomain>
2020-12-20 22:52:04 -0300

    Modificación a RBT para iteración en inserción.

--- libs/RBT.cpp
+++ libs/RBT.cpp
@@ -79,159 +79,164 @@ void RBT::insertFixup(RBT_node * node){
 	// Pasos > 1 en adelante de 
 	// https://www.geeksforgeeks.org/red-black-tree-set-2-insert/
 
-	// Paso 2
-	if(node->value == root->value){
-		node->isBlack = true;
-		treeBlackHeight++;
-		return;
-	}
-	// Paso 3
-	if(!node->parent->isBlack){
-		// Paso 3.a
-		// Por propiedades del RedBlackTree:
-		// si el nodo padre es rojo entonces debe tener abuelo
-		RBT_node* uncle = getUncle(node);
-		if(uncle != NULL && !uncle->isBlack){
-			node->parent->isBlack = true;
-			uncle->isBlack = true;
-			node->parent->parent->isBlack = false;
-			insertFixup(node->parent->parent);
+	while(node != NULL)	{
+		// Paso 2
+		if(node->value == root->value){
+			node->isBlack = true;
+			treeBlackHeight++;
 			return;
 		}
-		if(uncle != NULL && uncle->isBlack || !node->parent->isBlack){
-			RBT_node* p = node->parent;
-			RBT_node* g = p->parent;
-			if(p->value < g->value){
-				// p es hijo izquierdo de g
-				if(node->value < p->value){
-					// x es hijo izquierdo de p
-					// Rotación LL
-					
-					// Esta rotación es equivalente a la rightRotation de NodeBT
-					// Que no incluye la actualización de los enlaces a los padres
-					// Hacer la rotación y actualización de punteros a padres
-					if(g->parent == NULL){
-						// El padre de g es la raíz
-						root = (RBT_node*) g->Node_rightRotation();
-						root->parent = NULL;
-					}else{
-						// El padre de g es un nodo
-						if(g->value < g->parent->value){
-							// g es hijo izquerdo
-							g->parent->leftChild = (RBT_node*) g->Node_rightRotation();
-						}else{
-							// g es hijo derecho
-							g->parent->rightChild = (RBT_node*) g->Node_rightRotation();
-						}
-						p->parent = g->parent;
-					}
-					g->parent = p;
-					if(g->leftChild != NULL){
-						((RBT_node*)g->leftChild)->parent = g;
-					}
-					g->isBlack = false;
-					p->isBlack = true;
-					node->isBlack = false;
-				}else{
-					// x es el hijo derecho de p
-					// Rotación LR
-					// 1° Rotación a la izquierda en P
-					g->leftChild = (RBT_node*) p->Node_leftRotation();
-					if(p->rightChild != NULL){
-						((RBT_node*) p->rightChild)->parent = p;
-					}
-					p->parent = node;
-					node->parent = g;
-					// 2° Rotación a la derecha en G
-					if(g->parent == NULL){
-						// El padre de g es la raíz
-						root = (RBT_node*) g->Node_rightRotation();
-						root->parent = NULL;
-					}else{
-						// El padre de g es un nodo
-						if(g->value < g->parent->value){
-							// g es hijo izquerdo
-							g->parent->leftChild = (RBT_node*) g->Node_rightRotation();
-						}else{
-							// g es hijo derecho
-							g->parent->rightChild = (RBT_node*) g->Node_rightRotation();
-						}
-						node->parent = g->parent;
-					}
-					g->parent = node;
-					if(g->leftChild != NULL){
-						((RBT_node*)g->leftChild)->parent = g;
-					}
-					g->isBlack = false;
-					p->isBlack = false;
-					node->isBlack = true;
-				}
+		// Paso 3
+		if(!node->parent->isBlack){
+			// Paso 3.a
+			// Por propiedades del RedBlackTree:
+			// si el nodo padre es rojo entonces debe tener abuelo
+			RBT_node* uncle = getUncle(node);
+			if(uncle != NULL && !uncle->isBlack){
+				node->parent->isBlack = true;
+				uncle->isBlack = true;
+				node->parent->parent->isBlack = false;
+				node = node->parent->parent;
 			}else{
-				// p es hijo derecho de g
-				if(node->value > p->value){
-					// x es hijo derecho de p
-					// Rotación RR
-				
-					// Esta rotación es equivalente a la leftRotation de NodeBT
-					// Que no incluye la actualización de los enlaces a los padres
-					// Hacer la rotación y actualización de punteros a padres
-					if(g->parent == NULL){
-						// El padre de g es la raíz
-						root = (RBT_node*) g->Node_leftRotation();
-						root->parent = NULL;
-					}else{
-						// El padre de g es un nodo
-						if(g->value < g->parent->value){
-							// g es hijo izquerdo
-							g->parent->leftChild = (RBT_node*) g->Node_leftRotation();
+				if(uncle != NULL && uncle->isBlack || !node->parent->isBlack){
+					RBT_node* p = node->parent;
+					RBT_node* g = p->parent;
+					if(p->value < g->value){
+						// p es hijo izquierdo de g
+						if(node->value < p->value){
+							// x es hijo izquierdo de p
+							// Rotación LL
+							
+							// Esta rotación es equivalente a la rightRotation de NodeBT
+							// Que no incluye la actualización de los enlaces a los padres
+							// Hacer la rotación y actualización de punteros a padres
+							if(g->parent == NULL){
+								// El padre de g es la raíz
+								root = (RBT_node*) g->Node_rightRotation();
+								root->parent = NULL;
+							}else{
+								// El padre de g es un nodo
+								if(g->value < g->parent->value){
+									// g es hijo izquerdo
+									g->parent->leftChild = (RBT_node*) g->Node_rightRotation();
+								}else{
+									// g es hijo derecho
+									g->parent->rightChild = (RBT_node*) g->Node_rightRotation();
+								}
+								p->parent = g->parent;
+							}
+							g->parent = p;
+							if(g->leftChild != NULL){
+								((RBT_node*)g->leftChild)->parent = g;
+							}
+							g->isBlack = false;
+							p->isBlack = true;
+							node->isBlack = false;
 						}else{
-							// g es hijo derecho
-							g->parent->rightChild = (RBT_node*) g->Node_leftRotation();
+							// x es el hijo derecho de p
+							// Rotación LR
+							// 1° Rotación a la izquierda en P
+							g->leftChild = (RBT_node*) p->Node_leftRotation();
+							if(p->rightChild != NULL){
+								((RBT_node*) p->rightChild)->parent = p;
+							}
+							p->parent = node;
+							node->parent = g;
+							// 2° Rotación a la derecha en G
+							if(g->parent == NULL){
+								// El padre de g es la raíz
+								root = (RBT_node*) g->Node_rightRotation();
+								root->parent = NULL;
+							}else{
+								// El padre de g es un nodo
+								if(g->value < g->parent->value){
+									// g es hijo izquerdo
+									g->parent->leftChild = (RBT_node*) g->Node_rightRotation();
+								}else{
+									// g es hijo derecho
+									g->parent->rightChild = (RBT_node*) g->Node_rightRotation();
+								}
+								node->parent = g->parent;
+							}
+							g->parent = node;
+							if(g->leftChild != NULL){
+								((RBT_node*)g->leftChild)->parent = g;
+							}
+							g->isBlack = false;
+							p->isBlack = false;
+							node->isBlack = true;
 						}
-						p->parent = g->parent;
-					}
-					g->parent = p;
-					if(g->rightChild != NULL){
-						((RBT_node*)g->rightChild)->parent = g;
-					}
-					g->isBlack = false;
-					p->isBlack = true;
-					node->isBlack = false;
-				}else{
-					// x es el hijo izquierdo de p
-					// Rotación RL
-					// 1° Rotación a la derecha en p
-					g->rightChild = (RBT_node*) p->Node_rightRotation();
-					if(p->leftChild != NULL){
-						((RBT_node*) p->leftChild)->parent = p;
-					}
-					p->parent = node;
-					node->parent = g;
-					// 2° Rotación a la izquierda en G
-					if(g->parent == NULL){
-						// El padre de g es la raíz
-						root = (RBT_node*) g->Node_leftRotation();
-						root->parent = NULL;
 					}else{
-						// El padre de g es un nodo
-						if(g->value < g->parent->value){
-							// g es hijo izquerdo
-							g->parent->leftChild = (RBT_node*) g->Node_leftRotation();
+						// p es hijo derecho de g
+						if(node->value > p->value){
+							// x es hijo derecho de p
+							// Rotación RR
+						
+							// Esta rotación es equivalente a la leftRotation de NodeBT
+							// Que no incluye la actualización de los enlaces a los padres
+							// Hacer la rotación y actualización de punteros a padres
+							if(g->parent == NULL){
+								// El padre de g es la raíz
+								root = (RBT_node*) g->Node_leftRotation();
+								root->parent = NULL;
+							}else{
+								// El padre de g es un nodo
+								if(g->value < g->parent->value){
+									// g es hijo izquerdo
+									g->parent->leftChild = (RBT_node*) g->Node_leftRotation();
+								}else{
+									// g es hijo derecho
+									g->parent->rightChild = (RBT_node*) g->Node_leftRotation();
+								}
+								p->parent = g->parent;
+							}
+							g->parent = p;
+							if(g->rightChild != NULL){
+								((RBT_node*)g->rightChild)->parent = g;
+							}
+							g->isBlack = false;
+							p->isBlack = true;
+							node->isBlack = false;
 						}else{
-							// g es hijo derecho
-							g->parent->rightChild = (RBT_node*) g->Node_leftRotation();
-						}
-						node->parent = g->parent;
+							// x es el hijo izquierdo de p
+							// Rotación RL
+							// 1° Rotación a la derecha en p
+							g->rightChild = (RBT_node*) p->Node_rightRotation();
+							if(p->leftChild != NULL){
+								((RBT_node*) p->leftChild)->parent = p;
+							}
+							p->parent = node;
+							node->parent = g;
+							// 2° Rotación a la izquierda en G
+							if(g->parent == NULL){
+								// El padre de g es la raíz
+								root = (RBT_node*) g->Node_leftRotation();
+								root->parent = NULL;
+							}else{
+								// El padre de g es un nodo
+								if(g->value < g->parent->value){
+									// g es hijo izquerdo
+									g->parent->leftChild = (RBT_node*) g->Node_leftRotation();
+								}else{
+									// g es hijo derecho
+									g->parent->rightChild = (RBT_node*) g->Node_leftRotation();
+								}
+								node->parent = g->parent;
+							}
+							g->parent = node;
+							if(g->rightChild != NULL){
+								((RBT_node*)g->rightChild)->parent = g;
+							}
+							g->isBlack = false;
+							p->isBlack = false;
+							node->isBlack = true;					
+						}				
 					}
-					g->parent = node;
-					if(g->rightChild != NULL){
-						((RBT_node*)g->rightChild)->parent = g;
-					}
-					g->isBlack = false;
-					p->isBlack = false;
-					node->isBlack = true;					
-				}				
+				}
+				return;
 			}
+		}else{
+			return;
 		}
 	}
 }
